45f91fe80f8ee9304178095a98142ab281a736dc
diciommoriccardo <riccardo.diciommo1@gmail.com>
2021-12-22 12:48:57 +0100

    server startup

---
+++ main.go
@@ -0,0 +1,19 @@
+package everywheretew
+
+import (
+	routes "everywheretew/src/helpers"
+)
+
+/*func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}*/
+
+func main() {
+	routes.SetAllRoutes()
+}
