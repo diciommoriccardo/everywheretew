768298d6bf3a4fc247090b81cd5f44bcf8224b42
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-07 12:13:32 +0100

    changed connection methods

--- mongo.go
+++ mongo.go
@@ -12,7 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func ConnectToCluster() {
+func ConnectToApiCluster() *mongo.Collection {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
@@ -28,10 +28,10 @@ func ConnectToCluster() {
 	}
 
 	defer client.Disconnect(ctx)
-	twitterCollection := client.Database("api").Collection("twitter-data")
-	//clusterCollection := client.Database("everywheretew").Collection("cluster")
-	//userCollection := client.Database("everywheretew").Collection("users")
-	docCount, CollectionErr := twitterCollection.CountDocuments(ctx, bson.D{})
+	ApiCollection := client.Database("api").Collection("twitter-data")
+	// ClusterCollection := client.Database("everywheretew").Collection("cluster")
+	// UserCollection := client.Database("everywheretew").Collection("users")
+	docCount, CollectionErr := ApiCollection.CountDocuments(ctx, bson.D{})
 
 	if CollectionErr != nil {
 		fmt.Println("ERROR")
@@ -39,4 +39,61 @@ func ConnectToCluster() {
 	}
 
 	fmt.Println(docCount)
+	return ApiCollection
+}
+
+func ConnectTo_Cluster() *mongo.Collection {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	uri := "mongodb+srv://cluster-everywheretew.4ulev.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=bin/mongoCert.pem"
+	clientOpts := options.Client().ApplyURI(uri)
+	client, ConnectionError := mongo.Connect(ctx, clientOpts)
+	err := client.Ping(ctx, readpref.Primary())
+
+	if ConnectionError != nil {
+		fmt.Println("ERROR")
+		log.Fatal(err)
+		log.Fatal(ConnectionError)
+	}
+
+	defer client.Disconnect(ctx)
+	ClusterCollection := client.Database("everywheretew").Collection("cluster")
+	docCount, CollectionErr := ClusterCollection.CountDocuments(ctx, bson.D{})
+
+	if CollectionErr != nil {
+		fmt.Println("ERROR")
+		log.Fatal(CollectionErr)
+	}
+
+	fmt.Println(docCount)
+	return ClusterCollection
+}
+
+func ConnectToUserCluster() *mongo.Collection {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	uri := "mongodb+srv://cluster-everywheretew.4ulev.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&tlsCertificateKeyFile=bin/mongoCert.pem"
+	clientOpts := options.Client().ApplyURI(uri)
+	client, ConnectionError := mongo.Connect(ctx, clientOpts)
+	err := client.Ping(ctx, readpref.Primary())
+
+	if ConnectionError != nil {
+		fmt.Println("ERROR")
+		log.Fatal(err)
+		log.Fatal(ConnectionError)
+	}
+
+	defer client.Disconnect(ctx)
+	UserCollection := client.Database("everywheretew").Collection("users")
+	docCount, CollectionErr := UserCollection.CountDocuments(ctx, bson.D{})
+
+	if CollectionErr != nil {
+		fmt.Println("ERROR")
+		log.Fatal(CollectionErr)
+	}
+
+	fmt.Println(docCount)
+	return UserCollection
 }
