6da65fc0e33fd6afc5e1e82b709d1cf3c0108b6a
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-09 19:07:48 +0100

    routes file

---
+++ routes.go
@@ -0,0 +1,45 @@
+package main
+
+import "net/http"
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		homePage,
+	},
+	Route{
+		"Get cluster",
+		"GET",
+		"/api/cluster/{id}",
+		getCluster,
+	},
+	Route{
+		"Add order data",
+		"POST",
+		"/api/order",
+		GetOrderData,
+	},
+	Route{
+		"Add product data",
+		"POST",
+		"/api/products",
+		GetProductData,
+	},
+	Route{
+		"Get all cluster",
+		"GET",
+		"/api/cluster/all",
+		getAllCluster,
+	},
+}
