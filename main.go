7c0eb641dd6ec7ce2de9bd0dd968138f49924329
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-09 19:07:12 +0100

    changed main

--- main.go
+++ main.go
@@ -4,24 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello homepage")
 }
 
-func Router() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homePage)
-	router.HandleFunc("/api/order", GetOrderData).Methods("POST")
-	router.HandleFunc("/api/products", GetProductData).Methods("POST")
-	router.HandleFunc("/api/cluster/{id}", getCluster).Methods("GET")
-	log.Fatal(http.ListenAndServe(":10000", router))
-}
-
 func main() {
-	fmt.Println("EverywhereTew - Automated Decision")
-	Router()
+	router := NewRouter()
+
+	log.Fatal(http.ListenAndServe(":10000", router))
 }
