a42698ab8723a35922d2d3375a698887c78b84b4
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-13 17:48:04 +0100

    Added cors policy

--- cluster.go
+++ cluster.go
@@ -51,6 +51,7 @@ func getAllCluster(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		panic(err)
