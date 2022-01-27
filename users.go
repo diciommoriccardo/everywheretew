d057c04b7d4c837184f6554e6427b6634e2a6c51
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-27 11:36:57 +0100

    fixed headers

--- users.go
+++ users.go
@@ -29,6 +29,7 @@ func GetUserData(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(result); err != nil {
 		panic(err)
