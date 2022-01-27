9883d28f14c8dd1a2c55ac8d7623ca5955793ec6
diciommoriccardo <riccardo.diciommo1@gmail.com>
2022-01-27 11:38:41 +0100

    added file for cron job

---
+++ common/cron.go
@@ -0,0 +1,19 @@
+package common
+
+import (
+	"fmt"
+
+	"github.com/robfig/cron/v3"
+)
+
+var C = cron.New()
+
+func init() {
+	C.AddFunc("@every 10s", tweetJob)
+	C.Start()
+}
+
+func tweetJob() {
+	fmt.Println("test")
+	//TweetAnalysis()
+}
